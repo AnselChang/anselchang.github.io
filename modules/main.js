5724a53c99820a9860c6a66151bb80110ca0103b
Ansel Chang <anselchang@Ansels-MacBook-Air.local>
2022-01-18 00:23:03 -0500

    good bugfix

--- modules/main.js
+++ modules/main.js
@@ -1,4 +1,4 @@
-import { loadImage, drawImage, drawCircle } from './graphics.js';
+//import { loadImage, drawImage, drawCircle } from './graphics.js';
 
 var KEYS = {
   "UP" : 38,
@@ -6,6 +6,36 @@ var KEYS = {
   "LEFT" : 37,
   "RIGHT" : 39,
   "SPACEBAR" : 32
+}
+
+function loadImage(filename, width, height) {
+  let img;
+  if (width === undefined || height == undefined) {
+    img = new Image();
+  } else {
+    img = new Image(width , height);
+  }
+  img.src = filename;
+  return img;
+}
+
+// draw rotated image
+// https://stackoverflow.com/questions/17411991/html5-canvas-rotate-image
+function drawImage(ctx, image, x, y, cx, cy, rotation, scale = 1) {
+    ctx.setTransform(scale, 0, 0, scale, x, y); // sets scale and origin
+    ctx.rotate(rotation);
+    ctx.drawImage(image, -cx*image.width, -cy*image.height);
+    ctx.setTransform(1,0,0,1,0,0);
+}
+
+function drawCircle(context, centerX, centerY, radius, color = 'black') {
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = color;
+  context.fill();
+  context.lineWidth = 5;
+  context.strokeStyle = color;
+  context.stroke();
 }
 
 class Player {
